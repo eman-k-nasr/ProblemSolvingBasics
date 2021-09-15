3f10c15eb20738250b6ecb25741b4f9c9c0950ac
eman <emankamalnasr96@gmail.com>
2021-09-15 17:32:43 +0200

    diagonal-difference

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## easy 
 1. [solve me first](https://www.hackerrank.com/challenges/solve-me-first/problem)
 1. [simple array sum](https://www.hackerrank.com/challenges/simple-array-sum/problem)
+1. [compare triplets](https://www.hackerrank.com/challenges/compare-the-triplets/problem)
+1. [very big sum](https://www.hackerrank.com/challenges/a-very-big-sum/problem) 
---
+++ src/easy/diagonal_difference/Solution.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package easy.diagonal_difference;
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+/**
+ *
+ * @emankamal
+ */
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("diagonal differences"));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
